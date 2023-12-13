2838c4fa591ffc5009bdb7d5dacd59ee5e97bf1d
naresh <naresh@my.lab>
2023-12-13 18:56:21 +0530

    Working_with_text_mode

--- actions/actions.py
+++ actions/actions.py
@@ -1,60 +1,61 @@
-from typing import Any, Text, Dict, List
+from typing import Text, List, Any, Dict
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import FollowupAction , Restarted
 
-class ActionProvideInfrastructureOptions(Action):
+class ActionAskForSupport(Action):
     def name(self) -> Text:
-        return "action_provide_infrastructure_options"
+        return "action_ask_for_support"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # dispatcher.utter_message(text="Please select the type of infrastructure:",
-        #                          buttons=[
-        #                              {"title": "Cloud Infra", "payload": "/choose_infrastructure{\"infrastructure\":\"cloud_infra\"}"},
-        #                              {"title": "Physical Infra", "payload": "/choose_infrastructure{\"infrastructure\":\"physical_infra\"}"}
-        #                          ])
-        # return []
-        message = {
-            "payload": "dropDown",
-            "data": [
-                {"label": "Cloud Infra", "value": "/choose_infrastructure{\"infrastructure\":\"cloud_infra\"}"},
-                {"label": "Physical Infra", "value": "/choose_infrastructure{\"infrastructure\":\"physical_infra\"}"}
-            ]
-        }
-        dispatcher.utter_message(json_message=message)
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("How may I assist you? Please select below services which I offer.")
         return []
 
-
-class ActionProvideCloudInfraOptions(Action):
+class ActionSelectInfrastructure(Action):
     def name(self) -> Text:
-        return "action_provide_cloud_infra_options"
+        return "action_select_infrastructure"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        try:
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("You have selected Infrastructure.")
+        dispatcher.utter_message("Report Production Issue\nCloud VM provisioning")
+        return []
 
-            dispatcher.utter_message(text="Please choose a cloud infrastructure option:",
-                                    buttons=[
-                                        {"title": "Public Cloud", "payload": "/choose_cloud_infra{\"cloud_infra\":\"public_cloud\"}"},
-                                        {"title": "Private Cloud", "payload": "/choose_cloud_infra{\"cloud_infra\":\"private_cloud\"}"},
-                                        {"title": "Hybrid Cloud", "payload": "/choose_cloud_infra{\"cloud_infra\":\"hybrid_cloud\"}"}
-                                    ])
-            return []
-        except Exception as e:
-            # Log the exception if needed (e.g., print it or send it to a logging service)
-            dispatcher.utter_message(text="Something went wrong, please contact Support.")
-            # Optionally, you can reset the conversation by triggering the action_restart action
-            return [FollowupAction("action_restart")]
-        
-class ActionRestart(Action):
+class ActionSelectDocumentation(Action):
     def name(self) -> Text:
-        return "action_restart"
+        return "action_select_documentation"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        return [Restarted()]
-    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("You have selected Support Documentation.")
+        dispatcher.utter_message("Shift Rota\nSupport Contacts")
+        return []
+
+class ActionCloudVMProvisioning(Action):
+    def name(self) -> Text:
+        return "action_cloud_vm_provisioning"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("You have selected Cloud VM provisioning.")
+        return []
+
+class ActionReportProductionIssue(Action):
+    def name(self) -> Text:
+        return "action_report_production_issue"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("You have selected Infrastructure.\nReport Production Issue")
+        return []
+
+class ActionProvideShiftRota(Action):
+    def name(self) -> Text:
+        return "action_provide_shift_rota"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("You can find the Shift Rota information [here](https://chat.bing.com).")
+        return []
+
+class ActionProvideSupportContacts(Action):
+    def name(self) -> Text:
+        return "action_provide_support_contacts"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("You can find the Support Contacts [here](https://chat.openai.com).")
+        return []
